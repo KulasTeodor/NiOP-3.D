e60e773c118a58292a52b1c0139fedc3340a2c93
KulasTeodor <44947886+KulasTeodor@users.noreply.github.com>
2018-11-28 11:22:17 +0100

    Add files via upload

---
+++ KulasTeodorTimeSpanZadatak/KulasTeodorTimeSpanZadatak/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KulasTeodorTimeSpanZadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime novaGod = new DateTime(DateTime.Now.Year + 1, 1, 1);
+            TimeSpan preostalo = novaGod - DateTime.Now;
+            Console.WriteLine("Do Nove godine preostalo je:");
+            Console.WriteLine("{0} dana, {1}sati, {2}minuta , {3} sekundi",preostalo.Days,preostalo.Hours,preostalo.Minutes,preostalo.Seconds);
+            Console.WriteLine("U danima: {0}", preostalo.TotalDays);
+            Console.WriteLine("U satima: {0}", preostalo.TotalHours);
+            Console.WriteLine("U min.: {0}", preostalo.TotalMinutes);
+            Console.WriteLine("U sek.: {0}", preostalo.TotalSeconds);
+            Console.ReadKey();
+        }
+    }
+}
