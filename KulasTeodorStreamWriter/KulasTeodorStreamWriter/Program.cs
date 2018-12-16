efdf451e7cc8bdf7c272a451e5788127381ede05
KulasTeodor <44947886+KulasTeodor@users.noreply.github.com>
2018-12-16 12:12:15 +0100

    Add files via upload

---
+++ KulasTeodorStreamWriter/KulasTeodorStreamWriter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KulasTeodorStreamWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite ime: ");
+            string ime = Console.ReadLine();
+
+            Console.Write("Unesite prezime: ");
+            string prezime = Console.ReadLine();
+
+            StreamWriter sw = new StreamWriter(@"D:\My Documents\NOOP\datoteka.txt");
+            sw.WriteLine("Ime: {0}", ime);
+            sw.WriteLine("Prezime: {0}", prezime);
+            sw.Close();
+        }
+    }
+}
