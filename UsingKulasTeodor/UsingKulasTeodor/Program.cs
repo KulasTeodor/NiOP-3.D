a7946185c7c258de2cab98f60d3213a259367ae7
KulasTeodor <44947886+KulasTeodor@users.noreply.github.com>
2019-01-16 17:44:30 +0100

    Add files via upload

---
+++ UsingKulasTeodor/UsingKulasTeodor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingKulasTeodor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite ime: ");
+            string ime = Console.ReadLine();
+            Console.Write("Unesite prezime: ");
+            string prezime = Console.ReadLine();
+            // Podatke upisujemo u datoteku
+            Console.WriteLine("\n-- Zapisujemo u datoteku...");
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\Učenik\Desktop\Teodor Kulas,3.D\UsingKulasTeodor\datoteka.txt"))
+            {
+                sw.WriteLine("Ime: {0}", ime);
+                sw.WriteLine("Prezime: {0}", prezime);
+            }
+            // Podatke čitamo iz datoteke
+            Console.WriteLine("\n-- Pročitano iz datoteke:");
+            using (StreamReader sr = new StreamReader(@"C:\Users\Učenik\Desktop\Teodor Kulas,3.D\UsingKulasTeodor\datoteka.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
