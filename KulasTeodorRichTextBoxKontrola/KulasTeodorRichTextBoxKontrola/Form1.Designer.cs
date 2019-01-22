3237f9dae404b96e295ca6218235c9b18327e06e
KulasTeodor <44947886+KulasTeodor@users.noreply.github.com>
2019-01-22 16:17:27 +0100

    Add files via upload

---
+++ KulasTeodorRichTextBoxKontrola/KulasTeodorRichTextBoxKontrola/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace KulasTeodorRichTextBoxKontrola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonCitaj = new System.Windows.Forms.Button();
+            this.buttonBrisi = new System.Windows.Forms.Button();
+            this.buttonSpremi = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.teodorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(76, 99);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(218, 129);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // buttonCitaj
+            // 
+            this.buttonCitaj.Location = new System.Drawing.Point(81, 70);
+            this.buttonCitaj.Name = "buttonCitaj";
+            this.buttonCitaj.Size = new System.Drawing.Size(75, 23);
+            this.buttonCitaj.TabIndex = 1;
+            this.buttonCitaj.Text = "Citaj";
+            this.buttonCitaj.UseVisualStyleBackColor = true;
+            this.buttonCitaj.Click += new System.EventHandler(this.buttonCitaj_Click);
+            // 
+            // buttonBrisi
+            // 
+            this.buttonBrisi.Location = new System.Drawing.Point(219, 70);
+            this.buttonBrisi.Name = "buttonBrisi";
+            this.buttonBrisi.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrisi.TabIndex = 2;
+            this.buttonBrisi.Text = "Brisi";
+            this.buttonBrisi.UseVisualStyleBackColor = true;
+            this.buttonBrisi.Click += new System.EventHandler(this.buttonBrisi_Click);
+            // 
+            // buttonSpremi
+            // 
+            this.buttonSpremi.Location = new System.Drawing.Point(76, 247);
+            this.buttonSpremi.Name = "buttonSpremi";
+            this.buttonSpremi.Size = new System.Drawing.Size(75, 23);
+            this.buttonSpremi.TabIndex = 3;
+            this.buttonSpremi.Text = "Spremi";
+            this.buttonSpremi.UseVisualStyleBackColor = true;
+            this.buttonSpremi.Click += new System.EventHandler(this.buttonSpremi_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.teodorToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(407, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // teodorToolStripMenuItem
+            // 
+            this.teodorToolStripMenuItem.Name = "teodorToolStripMenuItem";
+            this.teodorToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.teodorToolStripMenuItem.Text = "O programu";
+            this.teodorToolStripMenuItem.Click += new System.EventHandler(this.teodorToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 336);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.buttonSpremi);
+            this.Controls.Add(this.buttonBrisi);
+            this.Controls.Add(this.buttonCitaj);
+            this.Controls.Add(this.richTextBox1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonCitaj;
+        private System.Windows.Forms.Button buttonBrisi;
+        private System.Windows.Forms.Button buttonSpremi;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem teodorToolStripMenuItem;
+    }
+}
+
---
+++ KulasTeodorRichTextBoxKontrola/KulasTeodorRichTextBoxKontrola/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KulasTeodorRichTextBoxKontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCitaj_Click(object sender, EventArgs e)
+        {
+            {
+                richTextBox1.LoadFile(@"C:Tempproba.rtf");
+            }
+
+        }
+
+        private void buttonBrisi_Click(object sender, EventArgs e)
+        {
+            {
+                richTextBox1.Clear();
+            }
+
+        }
+
+        private void buttonSpremi_Click(object sender, EventArgs e)
+        {
+            {
+                richTextBox1.SaveFile(@"C:Tempproba.rtf");
+                MessageBox.Show("Tekst spremljen");
+            }
+
+        }
+
+        private void teodorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Naziv programa: Rich Text Box Kontrola \nIme učenika: Teodor \nPrezime učenika: Kulaš \nDatum izrade: 21.1.2019");
+
+        }
+    }
+}
