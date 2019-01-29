18e6e49113c5214bb1957f39608fcd6f1fc16e22
KulasTeodor <44947886+KulasTeodor@users.noreply.github.com>
2019-01-29 09:11:13 +0100

    Add files via upload

---
+++ KulasTeodorMonthCalendarKontrola/KulasTeodorMonthCalendarKontrola/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KulasTeodorMonthCalendarKontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTermin_Click(object sender, EventArgs e)
+        {
+            // objektu klase TimeSpan dodjeljuje se izabrani broj dana
+            TimeSpan ts = monthCalendar1.SelectionEnd - monthCalendar1.SelectionStart;
+
+            // u slučaju da korisnik nije izabrao termin, pokreće se message box
+            // sa odgovarajucom porukom
+            if (ts.Days < 1)
+            {
+                MessageBox.Show("Korištenjem tipke SHIFT odaberite opseg datuma.");
+                return;
+            }
+
+            // poruka o izabranom broju dana
+            MessageBox.Show("Rezervirali ste usluge u trajanju od " + ts.Days.ToString() + "dana.", "Rezervacija");
+            // poruka o izabranim datumima pocetka i kraja rezervacije
+            MessageBox.Show("Vasa rezervacija počinje " + monthCalendar1.SelectionStart.ToShortDateString() + " a završava se " + monthCalendar1.SelectionEnd.ToShortDateString());
+
+        }
+    }
+}
