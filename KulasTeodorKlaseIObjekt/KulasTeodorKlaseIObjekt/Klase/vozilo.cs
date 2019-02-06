def95b1d7bfa4399d01e732e9b9d38e605e8c705
KulasTeodor <44947886+KulasTeodor@users.noreply.github.com>
2019-02-06 12:11:26 +0100

    Add files via upload

---
+++ KulasTeodorKlaseIObjekt/KulasTeodorKlaseIObjekt/Klase/vozilo.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KulasTeodorKlaseIObjekt.Klase
+{
+    class Vozilo
+    {
+        int brojKotaca;
+        bool leti, pliva, vozi;
+        string vrsta;
+        string oznaka;
+
+        public void setBrojKotaca(int brojKotaca)
+        {
+            this.brojKotaca = brojKotaca;
+        }
+
+        public int getBrojKotaca()
+        {
+            return this.brojKotaca;
+        }
+
+        public void setLeti(bool leti)
+        {
+            this.leti = leti;
+        }
+        public bool getLeti()
+        {
+            return this.leti;
+        }
+
+        public void setPliva(bool pliva)
+        {
+            this.pliva = pliva;
+        }
+
+    
+        public bool getPliva()
+        {
+            return this.pliva;
+        }
+
+        public void setVrsta(string vrsta)
+        {
+            this.vrsta = vrsta;
+        }
+
+        public string getVrsta()
+        {
+            return this.vrsta;
+        }
+
+        public void setOznaka(string oznaka)
+        {
+            this.oznaka = oznaka;
+        }
+
+        public string getOznaka()
+        {
+            return this.oznaka;
+        }
+
+        public void setVozi(string oznaka)
+        {
+            this.vozi = vozi;
+        }
+
+        public bool getVozi()
+        {
+            return this.vozi;
+        }
+
+        public Vozilo(int brojKotaca, bool pliva, bool vozi, string vrsta, string oznaka)
+        {
+            this.brojKotaca = brojKotaca;
+            this.leti = leti;
+            this.pliva = pliva;
+            this.vozi = vozi;
+            this.vrsta = vrsta;
+            this.oznaka = oznaka;
+        }
+
+        public Vozilo()
+        {
+        }
+
+        public override string ToString()
+        {
+            return base.ToString()
+                {
+                return "Vrsta: " + vrsta + "Oznaka : " + oznaka + "\n Pliva" + pliva + "\n Leti" + leti + "\n Vozi" + vozi + "\n broj kotaca: " + brojKotaca;
+            }
+
+
+        }
+    }
+}
---
+++ KulasTeodorKlaseIObjekt/KulasTeodorKlaseIObjekt/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KulasTeodorKlaseIObjekt.Klase;
+
+namespace KulasTeodorKlaseIObjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vozilo avijon = new Vozilo();
+
+            avijon.setBrojKotaca(6);
+            avijon.setLeti(true);
+            avijon.setPliva(false);
+            avijon.setVrsta("Cesna");
+            avijon.setOznaka("1312");
+            avijon.setVozi(true);
+
+            Console.WriteLine("Vrsta: " + avijon.getVrsta() + "Oznaka : " + avijon.getOznaka() + "\n Leti: " + avijon.getLeti() + "\n Vozi: " + avijon.getVozi() + "\n Pliva: "+ avijon.getPliva() + "\n BrojKotaca: " + avijon.getBrojKotaca());
+
+            Vozilo kamion = new Vozilo(8, false, false, true, "TAM", "RI1987AR");
+
+            Console.WriteLine(kamion.ToString);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
