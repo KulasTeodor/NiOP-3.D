8662920201ae817a4d52a37d804075b9a949e8da
KulasTeodor <44947886+KulasTeodor@users.noreply.github.com>
2019-02-27 18:08:37 +0100

    Add files via upload

---
+++ KulasTeodorZadatakZaKraj/KulasTeodorZadatakZaKraj/KlasaVozilo/Vozilo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KulasTeodorZadatakZaKraj.KlasaVozilo
+{
+    class Vozilo
+    {
+        int BrojTockova, maxBrzina;
+        string vrsta;
+
+        public void setBrojTockova(int brojKotaca)
+        {
+            this.BrojTockova = brojKotaca;
+        }
+
+        public int getBrojTockova()
+        {
+            return this.BrojTockova;
+        }
+
+        public void setVrsta(string vrsta)
+        {
+            this.vrsta = vrsta;
+        }
+
+        public string getVrsta()
+        {
+            return this.vrsta;
+        }
+
+        public void setmaxBrzina(int maxBrzina)
+        {
+            this.maxBrzina = maxBrzina;
+        }
+
+        public int getmaxBrzina()
+        {
+            return this.maxBrzina;
+        }
+
+        public override string ToString()
+        {
+            return "Vrsta: " + vrsta + "\nBroj tockova: " + BrojTockova + "\nMax brzina: " + maxBrzina;
+        }
+
+
+
+    }
+}
---
+++ KulasTeodorZadatakZaKraj/KulasTeodorZadatakZaKraj/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KulasTeodorZadatakZaKraj.KlasaVozilo;
+
+namespace KulasTeodorZadatakZaKraj
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Unesite zeljenu brzinu.");
+
+            Vozilo motocikl = new Vozilo();
+
+            motocikl.setBrojTockova(2);
+            motocikl.setVrsta("Cesna");
+            motocikl.setmaxBrzina(Convert.ToInt32(Console.ReadLine())); 
+
+            Vozilo automobil = new Vozilo();
+            automobil.setBrojTockova(4);
+            automobil.setVrsta("Cesna");
+            automobil.setmaxBrzina((int)(motocikl.getmaxBrzina() * 0.3));
+
+            Console.WriteLine("\nMotocikl:");
+            Console.WriteLine(motocikl.ToString());
+
+            Console.WriteLine("\nAutomobil:");
+            Console.WriteLine(automobil.ToString());
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
